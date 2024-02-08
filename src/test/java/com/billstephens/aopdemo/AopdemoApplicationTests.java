a0a4bb6b57b1271393e797862529fc840bc82827
BillStephens2022 <two4onebill@yahoo.com>
2024-02-08 06:50:45 -0500

    initial commit

---
+++ src/test/java/com/billstephens/aopdemo/AopdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billstephens.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
